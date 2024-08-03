e6823cb75722f6947b10007cb549c2aa3094281d
amasha <fernandothushini8@gmail.com>
2024-08-04 02:59:27 +0530

    writing HTML

--- site.php
+++ site.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
 </head>
+
 <body>
     <?php
-        echo("Hello, world!");
+    echo ("Hello, world!");
+    echo "echo 1";
+    echo "<h1>echo 2</h1>";
+    echo "<hr>";
+    echo "<p>echo 3</p>";
     ?>
 </body>
+
 </html>
