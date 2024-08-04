bd56ef57c492336c7e6a32a8bd75121c97e8e511
amasha <fernandothushini8@gmail.com>
2024-08-04 18:32:51 +0530

    better calculator

--- calculator.php
+++ calculator.php
@@ -24,5 +24,33 @@
     <br> <br>
 
     Answer: <?php echo $_GET["num1"] + $_GET["num2"] ?>
+
+    <!-- Better Calculator -->
+     <form action="calculator.php" method="post">
+        First No: <input type="number" step="0.1" name="num1"> <br>
+        OP: <input type="textbox" name="op"> <br>
+        Second No: <input type="number" step="0.1" name="num2"> <br>
+        
+        <input type="submit">
+     </form>
+     <br><br>
+
+     Answer: <?php
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $op = $_POST["op"];
+
+        if($op == "+") {
+            echo $num1 + $num2;
+        } elseif($op == "-") {
+            echo $num1 - $num2;
+        } elseif($op == "/") {
+            echo $num1 / $num2;
+        } elseif($op == "*") {
+            echo $num1 * $num2;
+        } else {
+            echo "Invalid Operation";
+        }
+     ?>
 </body>
 </html>
